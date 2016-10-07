a9e0dcdb39e746567761a0964948dcce91840dce
acarvalh <alexandra.oliveira@cern.ch>
2016-10-07 19:55:54 +0200

    first version

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # singleVLQ_wide
+
+The file 'copy_cards.py' copy the four cards of the template 'channel'_M800GeV_W10p in the other mass and width hiphoteses
+To run you do: 
+
+python copy_cards.py 'channel' 
+
+Where for example  'channel' = bWbj_YL
+  ==> note that the intermediary particle and chirality are already chosen with this name
+
+** You SHOULD have the template cards 'channel'_M800GeV_W10p 
+
+On those please quadruple check:
+
+ - The couplings that are on/of in the customize card. 
+   This is a version of the param_card.dat, buth only with the relevant parameters to be changed
+   All the other parameters are taken as the default of the model 
+
+ - The sintax in the the proc card
+
+=========================================================================================================
+
+rules about the cards: 
+
+ - In the proc_cards the name of the output folder should match the beggining of the name of the file
+ 
+ - The " QCD=1 QED=1 VLQ<=2 " means that I only want processes that have exactly 2 VLQ couplings inside
+   + one QCD coupling to make the additional jet
+
+ - The " / x y bp" means that I do NOT want the VLQ in the middle to be one of these
+
---
+++ copy_cards.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# RUN: python python copy_cards.py --channel bWbj_YL
+import os, sys, time,math
+import subprocess
+from optparse import OptionParser
+parser = OptionParser()
+
+parser.add_option("--channel", type="str", dest="chan", help="channel: it should contain particle and chirality")
+(options, args) = parser.parse_args()
+channel = options.chan
+#channel="bWbj_YL"
+
+mass=(800 , 1000 , 1400)
+width=(10 , 20 , 30) 
+
+base="_M800GeV_W10p"
+slash="/"
+toMass="_M"
+toWidth="GeV_W"
+pp="p"
+
+cust="_customizecards.dat"
+proc="_proc_card.dat"
+EM="_extramodels.dat"     
+run="_run_card.dat"
+
+for m in range(0,len(mass)):
+  for w in range(0,len(width)):
+    print str(mass[m])+"  "+str(width[w])+"  "+str(float(mass[m]*width[w]/100))
+    #
+    folderbase= channel+slash
+    folderbaseMW=channel+base
+    print folderbaseMW
+    filecustbase =  folderbase+folderbaseMW+slash+folderbaseMW+cust
+    fileprocbase =  folderbase+folderbaseMW+slash+folderbaseMW+proc
+    fileEMbase =  folderbase+folderbaseMW+slash+folderbaseMW+EM
+    filerunbase =  folderbase+folderbaseMW+slash+folderbaseMW+run
+    #
+    folder= channel+slash
+    folderMW=channel+toMass+str(mass[m])+toWidth+str(width[w])+"p"
+    procMW=folderMW+proc
+    filecust =  folder+folderMW+slash+folderMW+cust
+    fileproc =  folder+folderMW+slash+folderMW+proc
+    fileEM =  folder+folderMW+slash+folderMW+EM
+    filerun =  folder+folderMW+slash+folderMW+run
+    #
+    process = subprocess.Popen(["mkdir "+folder+folderMW],shell=True,stdout=subprocess.PIPE)
+    out = process.stdout.read()
+    process = subprocess.Popen(["cp "+fileprocbase+" "+fileproc+"; "+\
+                                "cp "+filecustbase+" "+filecust+"; "+\
+                                "cp "+fileEMbase+" "+fileEM+"; "+\
+                                "cp "+filerunbase+" "+filerun+"; "\
+                               ],shell=True,stdout=subprocess.PIPE)
+    out = process.stdout.read()
+    # 
+    print fileproc
+    with open(fileproc, 'r+') as f:
+       content = f.read()
+       f.seek(0)
+       f.truncate()
+       f.write(content.replace(folderbaseMW, folderMW))
+    print filecust
+    
+    with open(filecust, 'r+') as fp:
+       content = fp.read()
+       fp.seek(0)
+       fp.truncate()
+       fp.write(content.replace('800.0', str(mass[m])+".0"))
+    fp.close()
+    with open(filecust, 'r+') as fp:
+       content = fp.read()
+       fp.seek(0)
+       fp.truncate()
+       fp.write(content.replace('80.0', str(float(mass[m]*width[w]/100))))
+
